e9e75fe795cc521f32e914ba3289d3958ba12040
megachokobons <sugardoris1@outlook.com>
2021-06-06 17:58:53 +0200

    Second migration

--- EmployeeManager.DAL/Migrations/20210606155735_Authorisation.Designer.cs
+++ EmployeeManager.DAL/Migrations/20210606155735_Authorisation.Designer.cs
@@ -4,14 +4,16 @@ using EmployeeManager.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmployeeManager.DAL.Migrations
 {
     [DbContext(typeof(EmployeeManagerDbContext))]
-    partial class EmployeeManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210606155735_Authorisation")]
+    partial class Authorisation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
